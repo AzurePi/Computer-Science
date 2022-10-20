d1b1f1d455dafd2d6748bb45c9270d1c4acedf64
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2022-10-19 23:55:29 -0300

    Update arquivos.c

--- ATP II/Projetos/1/arquivos.c
+++ ATP II/Projetos/1/arquivos.c
@@ -1,63 +1,70 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(){
-	//declaração de variáveis
 	FILE *codigo, *palavra; //endereços dos arquivos de entrada
 	FILE *linha; //endereço do arquivo de saída
-	int N;
-	char pal[50];
-	int i;
+	int N; //código
+	char pal[50]; //palavra lida a ser escrita
+	int i, s; //contadores
+	char palast[5][50]; //vetor para armazenar as últimas cinco strings
 	
 	//abertura dos arquivos de entrada e saída
 	codigo = fopen("codigos.dat", "r");
 	palavra = fopen("palavras.dat", "r");
 	linha = fopen("linhas.dat", "w");
 	
-	//laço que escreverá o arquivo linhas.dat até que um dos arquivos de entrada chegue ao fim
-	while(!feof(codigo) && !feof(palavra))
-	{
-		fscanf(codigo, "%d", &N); //quantidade de palavras a serem escritas
+	//escreve o arquivo linhas.dat até que um dos arquivos de entrada chegue ao fim
+	while(fscanf(codigo, "%d", &N) != EOF && !feof(palavra))
+	{	
 		if(N<0)
-		{
-			fseek(palavra, N, SEEK_CUR); //move a leitura para trás, já que N é negativo
-			for(i = 0; i<N; i++)
+		{	
+			for(i = N+5; i<5; i++)
 			{
-				fscanf(palavra, "%s", pal); //lê a palavra em palavras.dat
-				fprintf(linha, "%s", pal); //imprime a palavra em linhas.dat
+				fprintf(linha, "%s ", palast[i]); //imprime em linhas.dat uma das cinco últimas palavras lidas
 			}
-			fprintf(linha, "\n"); //pula linha
 		}else if(N>0)
 		{
 			for(i = 0; i<N; i++)
 			{
-				fscanf(palavra, "%s", pal); //lê a palavra em palavras.dat
+				fscanf(palavra, " %s", pal); //lê a palavra em palavras.dat
 				fprintf(linha, "%s ", pal); //imprime a palavra em linhas.dat
+			
+				//armazena as última cinco palavras
+				for(s = 0; s<5; s++)
+				{	
+					if(s != 4)
+						strcpy(palast[s], palast[s+1]); //move as palavras que já estão em palast para trás
+					else
+						strcpy(palast[s], pal); //adiciona a palavra lida ao fim de palast
+				}
 			}
-			fprintf(linha, "\n"); //pula linha
 		}else //caso N = 0
-		{
-			fprintf(linha, "%d\n", 0);
-		}
+			fprintf(linha, "%d", 0);
+				
+		fprintf(linha, "\n");
 	}
 	
 	//imprime os códigos que faltam em linhas.dat
-	while(!feof(codigo))
-	{
-		fscanf(codigo, "%d", &N);
-		fprintf(palavra, "%d ", N);	
+	while(fscanf(codigo, "%d", &N) != EOF)
+	{	
+		fprintf(linha, "%d ", N);	
 	}
 	
 	//imprime as palavras que faltam em linhas de 5
-	while(!feof(palavra))
-	{
-		fscanf(palavra, "%s", pal);
+	i = 1;
+	while(fscanf(palavra, " %s", pal) != EOF)
+	{	
 		fprintf(linha, "%s ", pal);
-		i++;
 		if(i % 5 == 0)
-		{
 			fprintf(linha, "\n");
-		}
+		i++;
 	}
+	
+	fclose(codigo);
+	fclose(palavra);	
+	fclose(linha);
 	
 	return 0;
 }
