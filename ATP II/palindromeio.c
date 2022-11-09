32073e6bd4f0986c0bd672a48de8873b19841f36
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2022-11-09 19:21:02 -0300

    Add files via upload

---
+++ ATP II/palindromeio.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+
+
+
+int main(){
+	char ent[2001];
+	int k, i;
+	int res;
+	
+	scanf("%s", ent); //lê a palavra na linha
+	k = strlen(ent);
+	
+	
+	
+	
+
+	printf("%d", res);
+
+	return 0;
+}
