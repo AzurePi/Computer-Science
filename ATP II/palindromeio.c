8909ba21e94f220918265b3bea37bc760b1f3f14
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2022-11-13 19:51:43 -0300

    Update palindromeio.c

--- ATP II/palindromeio.c
+++ ATP II/palindromeio.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-
+int palindromo(char *pal, int inicio, int fim, int tam){
+	if(inicio == fim)
+		return tam;
+	if(*pal[inicio] == *pal[fim]) //se o primeiro caractere é igual ao último
+		tam++; //aumenta o tamanho do maior palíndromo
+	palindromo(pal+1, inicio+1, fim-1, tam);
+}
 
 
 
@@ -14,7 +20,7 @@ int main(){
 	scanf("%s", ent); //lê a palavra na linha
 	k = strlen(ent);
 	
-	
+	palindromo(ent, 0, k, 0);
 	
 	
 
