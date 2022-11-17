17455db17b927ecb8303575e2c5a1c92acfe1743
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2022-11-17 19:41:27 -0300

    Update palindromeio.c

--- ATP II/palindromeio.c
+++ ATP II/palindromeio.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
+int maior = 1; //o maior palindromeio é, por padrão, uma única letra
 
-//verifica se pal de tamanho k é ou não palíndromo
-int palindromo(char *pal, int k){
-	int i;
+//verifica se pal, que começa em s e termina em e, é ou não palíndromo, de forma recursiva
+int palindromo(char *pal, int s, int e){
 	
-	for(i = 0; i<k/2; i++) //verifica cada par inicio-fim de letras
-	{
-		if(pal[i] != pal[k-1-i])
-			return 0; //se em algum momento as letras divergirem, retorna que não é palíndromo
+	if(s == e)
+		return 1; //se há um único caractere, é palíndromo
+	if(pal[s] != pal[e])
+		return 0; //se a primeira e a última letra da string não batem, não é palíndromo
+	if(s < e + 1)
+		return palindromo(pal, s+1, e-1); //se há mais de dois caracteres na string, checa se a string entre eles também é palíndromo
+		
+	return 1;
+
+}
+
+//determina o tamanho do maior palindromeio em ent de tamanho k
+void palindromeio(char *ent, int k){
+	int i, j;
+	
+	//laços aninhados que farão com que cada substring de ent seja checada
+	for(i = 0; i < k; i++)
+	{	for(j = k; j >= 0; j--)
+		{
+			if(palindromo(ent, i, j)) //se for palíndromo
+				if(j - i + 1 > maior) //se o palíndromo foi maioO do que o maior que se conhece
+					maior = j - i + 1;
+		}
 	}
-	
-	return 1; //é palíndromo
 }
-
-//determina o tamanho do maior pelindromeio em ent de tamanho k
-int palindromeio(char *ent, int k){
-	
-	
-	
-
-}
-
 
 int main(){
 	char ent[2001];
@@ -32,7 +40,9 @@ int main(){
 	scanf("%s", ent); //lê a palavra na linha
 	k = strlen(ent);
 	
-	printf("%d", palindromeio());
+	palindromeio(ent, k);
+	
+	printf("%d", maior);
 
 	return 0;
 }
