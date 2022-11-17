22ecf307d1d8192c2d1db5addd5dafbd824d0cb2
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2022-11-17 19:54:18 -0300

    Update hanoi_diferente.c

--- ATP II/hanoi_diferente.c
+++ ATP II/hanoi_diferente.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
+void hanoi(){
+
+
+
+
+}
 
 int main(){
+	int N, K;
+	int **torre;
+	int i;
+	
+	scanf("%d %d", &N, &K);
+
+	torre = malloc(sizeof(int)*3);
+	for(i = 0; i<3; i++)
+		torre[i] = malloc(sizeof(int)*N);
+
+
+
+
 
 
 	return 0;
