d8614283cea8ae25be21581b5ee10029de1b113c
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2022-11-18 19:41:14 -0300

    Update hanoi_diferente.c

--- ATP II/hanoi_diferente.c
+++ ATP II/hanoi_diferente.c
@@ -1,57 +1,129 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void hanoi(int **torre, int mov, int org, int io, int aux, int ia, int dest, int id){
-	if(mov != 0) //se há movimentos a fazer
+int K; //quantidade de movimentos
+int torre[3] = {0}; //inicializa com uma torre vazia
+int *A, *B, *C; //endereço dos três pinos na torre
+
+void hanoi(int origem, int destino, int auxilio, int n){
+	int *io, *id; //endereço de origem, endereço de destino
+	int i;
+	
+	if(K != 0) //se há movimentos a fazer
 	{
-		torre[aux][ia] = torre[org][ia]; //move da origem para o auxiliar
-		torre[org][io] =  0; //seta a origem como vazia
-		mov--;
-		org = aux; //o pino auxiliar é a nova origem
-		aux = dest; //o pino destino é o novo auxiliar
-		hanoi(torre, mov, org, io+1, aux, ia+1, dest, id+1);
+		if(n == 1) //se estamos lidando com o primeiro disco
+		{	
+			torre[origem]--; //há um disco a menos no pino de origem
+			torre[destino]++; //há um disco a mais no pino de destino
+			
+			
+			//determinação do endereço de origem
+			if(origem == 0) //se a origem for o primeiro pino
+				io = A;
+			else if(origem == 1) //se a origem for o segundo pino
+				io = B;
+			else if(origem == 2) //se a orgiem for o terceiro pino
+				io = C;
+			
+			//deterinação do endereço de destino	
+			if(destino == 0) //se o destino for o primeiro pino
+				id = A;
+			else if(destino == 1) //se o destino for o segundo pino
+				id = B;
+			else if(destino == 2) //se o destino for o terceiro pino
+				id = C;
+			
+			
+			i = 0;
+			while(io[i] == 0) //enquanto não há disco
+				i++; //passa para a próxima posição
+			
+			io[i] = 0; //no endereço de origem não há mais pino
+			id[i] = 1; //no endereço de destino agora há pino
+			
+			K--;
+		}else{
+			hanoi(origem, auxilio, destino, n-1); //faça o processo
+			//pensando em um disco a menos e trocando a função de cada pino
+			hanoi(origem, destino, auxilio, 1); //faça o processo para 
+			//mover um único pino
+			hanoi(auxilio, destino, origem, n-1); //faça o processo
+			//pensando em um disco a menos e trocando a função de cada pino
+		}
 	}
 }
 
 int main(){
-	int N, K;
-	int **torre;
-	int i, j;
+	int N;
+	int i, flag = 0;
 	
 	scanf("%d %d", &N, &K);
 
-	//aloca dinamicamente o espaço da torre
-	torre = malloc(sizeof(int)*3);
-	for(i = 0; i<3; i++)
-		torre[i] = malloc(sizeof(int)*N);
-
-	//determina que os discos estão todos no primeiro pino
-	for(j = 0; j<N; j++)
-	{
-		torre[0][j] = j+1;
-		torre[1][j] = 0;
-		torre[2][j] = 0;
-	}
-
-	hanoi(torre, K, 0, 0, 1, 0, 2, 0); //executa k movimentos na torre
+	//aloca dinamicamente o espaço necessário para cada pino
+	A = malloc(N*sizeof(int));
+	B = malloc(N*sizeof(int));
+	C = malloc(N*sizeof(int));
 	
-	//imprimindo a torre
-	for(i = 0; i<3; i++)
+	torre[0] = N; //no primeiro pino, há N discos
+	
+	//determina que há discos no primeiro pino, mas não há discos nos demais
+	for(i = 0; i<N; i++)
 	{
-		for(j = 0; j<N; j++)
-    {
-        if(torre[i][j] != 0)
-          printf("%d ", torre[i][j]);
-    }
-    if(torre[i][j] == 0)
-        printf("0");
-		printf("\n");
+		A[i] = 1;
+		B[i] = 0;
+		C[i] = 0;
 	}
+
+	//executa movimentos na torre
+	hanoi(0, 2, 1, N); 
+	
+	//imprimindo o pino de origem
+	for(i = 0; i<N; i++)
+	{
+		if(A[i] == 1) //se há disco nessa posição do pino
+		{
+			printf("%d ", i+1);
+			flag = 1;
+		}
+	}
+	if(flag == 0) //se não houve disco em posição alguma
+		printf("0");
+		
+	printf("\n");
+	flag = 0; //zera flag para reaproveitá-la
+	
+	//imprimindo o pino auxiliar
+	for(i = 0; i<N; i++)
+	{
+		if(B[i] == 1) //se há disco nessa posição do pino
+		{
+			printf("%d ", i+1);
+			flag = 1;
+		}
+	}
+	if(flag == 0) //se não houve disco em posição alguma
+		printf("0");
+		
+	printf("\n");
+	flag = 0; //zera flag para reaproveitá-la
+	
+	//imprimindo o pino de destino
+	for(i = 0; i<N; i++)
+	{
+		if(C[i] == 1) //se há disco nessa posição do pino
+		{
+			printf("%d ", i+1);
+			flag = 1;
+		}
+	}
+	if(flag == 0) //se não houve disco em posição alguma
+		printf("0");
+	
 		
 	//libera o espaço alocado para a torre
-	for(i = 0; i<3; i++)
-		free(torre[i]);
-	free(torre);
-
+	free(A);
+	free(B);
+	free(C);
+	
 	return 0;
 }
