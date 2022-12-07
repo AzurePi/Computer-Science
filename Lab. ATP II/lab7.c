fa7a5f6a3342b0a9fb410b324a8487990d184b82
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2022-12-07 10:46:42 -0300

    Add files via upload

---
+++ Lab. ATP II/lab7.c
@@ -0,0 +1,5 @@
+/*
+Faça uma função que receba um vetor A de dez elmentos inteiros como parâmetro.
+Ao final dessa função, deverá ter sido gerado um vetor B contendo o fatorial de cada elemento de A.
+O vetor B deverá ser mostrado no programa principal.
+*/
