6d05e758e8a4ce8ab107cebfa79a71058db46671
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2022-12-14 09:45:23 -0300

    Update lab7.c

--- Lab. ATP II/lab7.c
+++ Lab. ATP II/lab7.c
@@ -1,5 +1,42 @@
 /*
-FaÁa uma funÁ„o que receba um vetor A de dez elmentos inteiros como par‚metro.
-Ao final dessa funÁ„o, dever· ter sido gerado um vetor B contendo o fatorial de cada elemento de A.
-O vetor B dever· ser mostrado no programa principal.
+Fa√ßa uma fun√ß√£o que receba um vetor A de dez elmentos inteiros como par√¢metro.
+Ao final dessa fun√ß√£o, dever√° ter sido gerado um vetor B contendo o fatorial de cada elemento de A.
+O vetor B dever√° ser mostrado no programa principal.
 */
+
+#include <stdio.h>
+
+int fatorial(int a){
+	int f = 1;
+	
+	if(a == 0)
+		return 1;
+	
+	while(a > 1)
+	{
+		f *= a;
+		a--;
+	}
+	
+	return f;
+}
+
+int main(){
+	int A[10], B[10];
+	int i;
+	
+	for(i = 0; i<10; i++)
+	{
+		printf("A[%d]: ", i);
+		scanf("%d", &A[i]);
+		B[i] = fatorial(A[i]);
+	}
+	
+	printf("\n");
+	
+	for(i = 0; i<10; i++)
+		printf("B[%d]: %d\n", i, B[i]);
+	printf("\n");
+	
+	return 0;
+}
