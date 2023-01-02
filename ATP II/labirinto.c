499deceb162cbba11cd5c011b56ec7169b8af9d6
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2023-01-02 18:45:36 -0300

    Update labirinto.c

--- ATP II/labirinto.c
+++ ATP II/labirinto.c
@@ -5,6 +5,7 @@ int **labirinto;
 int sol = 0; //assume que não há solução
 
 void ler_labirinto(int N);
+int valido(int N, int x, int y);
 void caminhar(int N, int C, int x, int y, int p);
 
 int main(){
@@ -25,7 +26,6 @@ void ler_labirinto(int N){
 	int i, j;
 	
 	labirinto = malloc(N * sizeof(int *));
-	
 	for(i = 0; i<N; i++)
 		labirinto[i] = malloc(sizeof(int));
 		
@@ -38,29 +38,35 @@ void ler_labirinto(int N){
 	return;
 }
 
+//verifica se não extrapolamos a borda e se o caminho está livre
+int valido(int N, int x, int y){
+	if(x >= 0 && y >= 0 && x < N && y < N)
+	{
+		if(labirinto[x][y] == 0)
+			return 1;
+	}
+	return 0;
+}
+
 void caminhar(int N, int C, int x, int y, int p){
 
 	//se chegamos ao final
-	if(x == N-1 && y == C && labirinto[x][y] == 0)
+	if(x == N-1 && y == C)
 	{
 		printf("%d\n", p);
 		sol = 1; //indica que encontramos uma solução
 		return;
 	}
 	
-	//se não extrapolamos a borda
-	if(x < N && y < N && x >= 0 && y >= 0 && sol == 0)
-	{
-		//e o caminho está livre
-		if(labirinto[x][y] == 0)
-		{
-			labirinto[x][y] = 1;
-			
-			caminhar(N, C, x+1, y, p+1);
-			caminhar(N, C, x, y+1, p+1);
-			caminhar(N, C, x, y-1, p+1);
-		}
-	}
+	//se extrapolamos a borda, ou se o caminho está bloquado
+	if(!valido(N, x, y) && sol != 0)
+		return;
+	
+	labirinto[x][y] = 1;
+		
+	caminhar(N, C, x+1, y, p+1);
+	caminhar(N, C, x, y+1, p+1);
+	caminhar(N, C, x, y-1, p+1);
 
 	return;
 }
