3e69169bc657f849129b8ac23dae74915c2e184e
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2023-01-16 14:56:48 -0300

    Update confusa.c

--- ATP II/confusa.c
+++ ATP II/confusa.c
@@ -24,15 +24,15 @@ void ordenar(float *vet, int N, int k){
 	int i, j;
 	float x;
 	
-	for(i = 2; i<N; i++)
+	for(i = 0; i<N; i++) //andando pelo vetor inteiro (a partir do segundo elemento)
 	{
-		x = vet[i]; //armazena o número sendo inserido
-		j = i-1; //a inserção começará por aqui
+		x = vet[i]; //armazena o número a ser inserido
+		j = i-1; //a análise para inserção começará por aqui
 		
 		//se estamos antes de ou em k, ordenamos de forma crescente
 		if(i <= k)
 		{
-			//encontramos a posição, dentre as anteriores,
+			//encontramos a posição, dentre as anteriores a x,
 			//em que x deveria estar
 			while(j >= 0 && vet[j] > x)
 			{
@@ -40,11 +40,11 @@ void ordenar(float *vet, int N, int k){
 				j--; //analisa o índice anterior
 			}
 			vet[j+1] = x; //insere x na posição adequada
-		}else //se estamos em ou depois de k, ordenamos de forma decrescente
+		}else //if(i > k) ordenamos de forma decrescente
 		{
-			//encontramos a posição, dentre as anteriores,
-			//em que x deveria estar
-			while(j >= 0 && vet[j] < x)
+			//encontramos a posição, dentre as anteriores a x mas
+			//posteriores a k, em que x deveria estar
+			while(j > k && vet[j] < x)
 			{
 				vet[j+1] = vet[j]; //move o próximo número para essa posição
 				j--; //analisa o índice anterior
