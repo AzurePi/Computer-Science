8826f3aae9f01016110a2fb2affc97ca51846bbb
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2023-03-23 14:23:24 -0300

    Update and rename Main.java to matrizes.java

--- POO/matrizes/src/matrizes.java
+++ POO/matrizes/src/matrizes.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class matrizes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Dimensões da matriz 1: ");
