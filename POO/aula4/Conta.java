00000983970b74742c069b99f433cc249e24281c
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2023-03-27 13:39:47 +0000

    updates Conta.java

--- POO/aula4/Conta.java
+++ POO/aula4/Conta.java
@@ -1,43 +1,47 @@
 package aula4;
 
 public class Conta {
-    private String[] nome;
-    private long cpf;
-    private double saldo;
-    private int numero;
-    private static final int AGENCIA = 1;
-    
-    public void setNome(String[] arg){
-        nome = arg;
-    }
-    
-    public void setCPF(long arg){
-        cpf = arg;
-    }
-    
-    public String[] getNome(){
-        return nome;
-    }
-    
-    public long getCPF(){
-        return cpf;
-    }
-    
-    public double getSaldo(){
-        return saldo;
-    }
-    
-    public int getNumero(){
-        return numero;
-    }
-    
-    public int getAgencia(){
-        return AGENCIA;
-    }
-    
-    //função depósito
-    
-    //função saque
-    
-    
+  private String[] nome;
+  private long cpf;
+  private double saldo = 0;
+  private int numero;
+  private static final int AGENCIA = 1;
+  
+  public void setNome(String[] arg){
+    nome = arg;
+  }
+  
+  public void setCPF(long arg){
+    cpf = arg;
+  }
+  
+  public String[] getNome(){
+    return nome;
+  }
+  
+  public long getCPF(){
+    return cpf;
+  }
+  
+  public double getSaldo(){
+    return saldo;
+  }
+  
+  public int getNumero(){
+    return numero;
+  }
+  
+  public int getAgencia(){
+    return AGENCIA;
+  }
+
+  public void depositar(double arg){
+    saldo += arg;
+    System.out.print("Saldo em conta: " + getSaldo());
+  }
+
+  public void sacar(double arg){
+    saldo -= arg;
+    System.out.print("Saldo em conta: " + getSaldo());
+  }
 }
