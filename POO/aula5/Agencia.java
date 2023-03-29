03e510a488d6bb791a2be67077b927bfc2aa94ae
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2023-03-29 11:37:32 -0300

    Update Agencia.java

--- POO/aula5/Agencia.java
+++ POO/aula5/Agencia.java
@@ -1,10 +1,13 @@
 package aula5;
+
 import aula4.Conta;
+import java.util.ArrayList;
 
 public class Agencia {
     private String nome;
     private int numero;
-    
+    private int numBanco;
+    private ArrayList<Conta> contas = new ArrayList<>();
     
 
     
