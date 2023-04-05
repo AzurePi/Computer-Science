ee93af879f6a48cf42d0382cb67c6d7e280c847c
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2023-04-05 19:52:55 -0300

    Added files made during aula 7

---
+++ POO/aula7/predio/Andar.java
@@ -0,0 +1,20 @@
+package aula7.predio;
+
+/**
+ *
+ * @author Azure
+ */
+public class Andar {
+    private final int numero;
+    
+    //construtor
+    public Andar(int n){
+        this.numero = n;
+    }
+
+    //getter
+    public int getNumero() {
+        return numero;
+    }
+
+}
