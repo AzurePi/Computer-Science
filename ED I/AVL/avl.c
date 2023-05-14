1ee31d90ccf8ca788011e03e0addbaf6f0d5fc53
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2023-05-14 18:16:19 -0300

    Update avl.c

--- ED I/AVL/avl.c
+++ ED I/AVL/avl.c
@@ -1,226 +1,214 @@
+////Pedro Benedicto de Melo Cardana
+
 // Implementação: árvore binária de busca balanceada (AVL)
 #include "avl.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-
 // Operações ---------------------------------------------
 
 // Aloca um No com valor x na memória e retorna seu endereço
 No *novoNo(int x) {
-	No *novo = malloc(sizeof(No));
-	novo->valor = x;
-	novo->esq = NULL;
-	novo->dir = NULL;
-	novo->altura = 0; // todo nó é inserido como folha, e toda folha tem altura 0
-	return novo;
+  No *novo = malloc(sizeof(No));
+  novo->valor = x;
+  novo->altura = 1; // todo nó é inserido como folha, e toda folha tem altura 1
+  novo->esq = NULL;
+  novo->dir = NULL;
+  return novo;
 }
 
 // Retorna a altura do No no endereço raiz
-// Retorna -1 se raiz é NULL
+// Retorna 0 se raiz é NULL
 int alturaNo(No *raiz) {
-	if (raiz == NULL)
-		return -1;
-	return raiz->altura;
+  if (raiz == NULL)
+    return 0;
+  return raiz->altura;
 }
 
 // Retorna o maior entre os dois valores
 int maior(int a, int b) {
-	if (a >= b)
-		return a;
-	return b;
+  if (a >= b)
+    return a;
+  return b;
 }
 
 // Retorna o fator de balanceamento do No no endereço raiz
 // Retorna 0 se raiz é NULL
 int calcularFator(No *raiz) {
-	if (raiz == NULL)
-		return 0;
-	return alturaNo(raiz->esq) - alturaNo(raiz->dir);
+  if (raiz == NULL)
+    return 0;
+  return alturaNo(raiz->esq) - alturaNo(raiz->dir);
 }
 
 // Operação de rotação à esquerda a partir do No em r
 No *rotacaoEsquerda(No *r) {
-	No *auxEsq, *auxDir;
+  No *auxEsq = r->esq;
+  No *aux = auxEsq->esq;
 
-	// armazenamos as subárvores
-	auxEsq = r->esq;
-	auxDir = r->dir;
+  // rotação
+  auxEsq->esq = r;
+  r->dir = aux;
 
-	// rotação
-	auxDir->esq = r; // o filho esquerdo da subárvore direita passa a ser r
-	r->dir = auxEsq; // o filho direito de r passa a ser o filho esquerdo
+  // recalculamos a altura de quem foi movimentado
+  r->altura = maior(alturaNo(r->esq), alturaNo(r->dir)) + 1;
+  auxEsq->altura = maior(alturaNo(auxEsq->esq), alturaNo(auxEsq->dir)) + 1;
 
-	// recalculamos a altura de quem foi movimentado
-	r->altura = maior(alturaNo(r->esq), alturaNo(r->dir));
-	auxEsq->altura = maior(alturaNo(auxEsq->esq), alturaNo(auxEsq->dir));
-
-	return auxEsq; // que passou a ser a nova raiz
+  return auxEsq; // retorna a nova raiz
 }
 
-// Operação de rotaão à esquerda a partir do No em r
+// Operação de rotação à esquerda a partir do No em r
 No *rotacaoDireita(No *r) {
-	No *auxEsq, *auxDir;
+  No *auxEsq = r->esq;
+  No *aux = auxEsq->dir;
 
-	// armazenamos as subárvores
-	auxEsq = r->esq;
-	auxDir = r->dir;
+  // rotação
+  auxEsq->dir = r;
+  r->esq = aux;
 
-	// rotação
-	auxEsq->dir = r; // o filho direito da subárvore esquerda passa a ser r
-	r->esq = auxDir; // o filho esquerdo de r passa a ser o filho direito
+  // recalculamos a altura de quem foi movimentado
+  r->altura = maior(alturaNo(r->esq), alturaNo(r->dir)) + 1;
+  auxEsq->altura = maior(alturaNo(auxEsq->esq), alturaNo(auxEsq->dir)) + 1;
 
-	// recalculamos a altura de quem foi movimentado
-	r->altura = maior(alturaNo(r->esq), alturaNo(r->dir));
-	auxEsq->altura = maior(alturaNo(auxEsq->esq), alturaNo(auxEsq->dir));
-
-	return auxEsq; // que passou a ser a nova raiz
+  return auxEsq; // retorna a nova raiz
 }
 
-// Composição de uma rotação à direita centrada no filho à direita, com de uma à esquerda centrada em r
+// Composição de uma rotação à direita centrada no filho à direita, com de uma à
+// esquerda centrada em r
 No *rotacaoDuplaEsquerda(No *r) {
-	r->dir = rotacaoDireita(r->dir);
-	return rotacaoEsquerda(r);
+  r->dir = rotacaoDireita(r->dir);
+  return rotacaoEsquerda(r);
 }
 
-// Composição de uma rotação à esquerda centrada no filho à esquerda com de uma à direita dentrada em r
+// Composição de uma rotação à esquerda centrada no filho à esquerda com de uma
+// à direita dentrada em r
 No *rotacaoDuplaDireita(No *r) {
-	r->esq = rotacaoEsquerda(r->esq);
-	return rotacaoDireita(r);
+  r->esq = rotacaoEsquerda(r->esq);
+  return rotacaoDireita(r);
 }
 
 // realiza o balanceamento do No em raiz
 No *balancear(No *raiz) {
-	int fator = calcularFator(raiz);
+  int fator = calcularFator(raiz);
+  int fatorEsq = calcularFator(raiz->esq);
+  int fatorDir = calcularFator(raiz->dir);
 
-	// rotação à esquerda
-	if (fator < -1 && calcularFator(raiz->dir) <= 0)
-		raiz = rotacaoEsquerda(raiz);
-	// rotação à direita
-	else if (fator > 1 && calcularFator(raiz->esq) >= 0)
-		raiz = rotacaoDireita(raiz);
-	// rotação dupla esquerda-direita
-	else if (fator > 1 && calcularFator(raiz->esq) < 0)
-		raiz = rotacaoDuplaDireita(raiz);
-	// rotação dupla direita-esquerda
-	else if (fator < -1 && calcularFator(raiz->dir) > 0)
-		raiz = rotacaoDuplaEsquerda(raiz);
+  // rotação à esquerda
+  if (fator < -1 && fatorDir <= 0)
+    raiz = rotacaoEsquerda(raiz);
+  // rotação à direita
+  else if (fator > 1 && fatorEsq >= 0)
+    raiz = rotacaoDireita(raiz);
+  // rotação dupla esquerda-direita
+  else if (fator > 1 && fatorEsq < 0)
+    raiz = rotacaoDuplaDireita(raiz);
+  // rotação dupla direita-esquerda
+  else if (fator < -1 && fatorDir > 0)
+    raiz = rotacaoDuplaEsquerda(raiz);
 
-	return raiz;
+  return raiz;
 }
 
 // Insere um No com valor x na árvore, a partir do endereço raiz
 No *inserir(No *raiz, int x) {
-	// se achamos um espaço vazio, inserimos
-	if (raiz == NULL)
-		return novoNo(x);
+  // se achamos um espaço vazio, inserimos
+  if (raiz == NULL)
+    return novoNo(x);
 
-	// se é menor, vamos para a esquerda
-	if (x < raiz->valor)
-		raiz->esq = inserir(raiz->esq, x);
-	// se é maior, vamos para a direita
-	else if (x > raiz->valor)
-		raiz->dir = inserir(raiz->dir, x);
+  // se é menor, vamos para a esquerda
+  if (x < raiz->valor)
+    raiz->esq = inserir(raiz->esq, x);
+  // se é maior, vamos para a direita
+  else if (x > raiz->valor)
+    raiz->dir = inserir(raiz->dir, x);
+  // se já existe
+  else
+    return raiz;
 
-	// na recursão, atualiza a altura de todos os nós entre a raíz e o novo nó e os balanceia de acordo
-	raiz->altura = maior(alturaNo(raiz->esq), alturaNo(raiz->dir)) + 1;
-	raiz = balancear(raiz);
+  // atualiza a altura do nó
+  raiz->altura = maior(alturaNo(raiz->esq), alturaNo(raiz->dir)) + 1;
 
-	return raiz;
+  return balancear(raiz); // rebalanceaia a árvore e retorna
 }
 
 // Busca o No de valor x e o remove da árvore
 // O valor de retorno existe para permitir a recursão
 No *remover(No *raiz, int x) {
-	//se chegamos ao fim da árvore
-	if (raiz == NULL)
-		return NULL;
+  // se chegamos ao fim da árvore
+  if (raiz == NULL)
+    return NULL;
 
-	// se encontramos o nó a ser removido
-	if (raiz->valor == x) {
-		// se é nó folha
-		if (raiz->esq == NULL && raiz->dir == NULL) {
-			free(raiz);
-			return NULL;
-		}
+  if (x < raiz->valor)
+    raiz->esq = remover(raiz->esq, x);
+  else if (x > raiz->valor)
+    raiz->dir = remover(raiz->dir, x);
+  else {
 
-		// se tem dois filhos
-		if (raiz->esq != NULL && raiz->dir != NULL) {
-			substituiMenorDireita(raiz, raiz->dir);
+    // se há uma ou nenhuma subárvore
+    if (raiz->esq == NULL || raiz->dir == NULL) {
+      // se há subárvore à esquerda, salva-a, do contrário, salva a à direita
+      No *aux = raiz->esq ? raiz->esq : raiz->dir;
 
-			raiz->esq = remover(raiz->esq, x);
-			return raiz;
-		// se tem apenas um filho
-		} else {
-			No *aux;
-			// filho à esquerda
-			if (raiz->esq != NULL)
-				aux = raiz->esq;
-			// filho à direita
-			else
-				aux = raiz->dir;
+      // se não há subárvores
+      if (aux == NULL) {
+        aux = raiz;
+        raiz = NULL;
 
-			free(raiz);
-			return aux;
-		}
-	// se não encontramos ainda quem queremos removes
-	} else {
-		// se x é menor que o atual, está à esquerda
-		if (x < raiz->valor)
-			raiz->esq = remover(raiz->esq, x);
-		// se x é maior que o atual, está à direita
-		else
-			raiz->dir = remover(raiz->dir, x);
-	}
+        // se há subárvores
+      } else
+        raiz = aux;
 
-	// na recursão, atualiza a altura de todos os nós entre a raíz e o novo nó e os balanceia de acordo
-	raiz->altura = maior(alturaNo(raiz->esq), alturaNo(raiz->dir)) + 1;
-	raiz = balancear(raiz);
+      free(aux);
 
-	return raiz;
+      // se há duas subárvores
+    } else
+      substituiMenorDireita(raiz, raiz->dir);
+  }
+
+  // atualiza a altura do nó
+  raiz->altura = maior(alturaNo(raiz->esq), alturaNo(raiz->dir)) + 1;
+
+  return balancear(raiz); // rebalanceaia a árvore e retorna
 }
 
 void substituiMenorDireita(No *raiz, No *dir) {
-	// se não há elemento à esquerda (elemento menor)
-	if (dir->esq == NULL) {
-		No *aux;
-		raiz->valor = dir->valor; // substituição
+  // se não há elemento à esquerda (elemento menor)
+  if (dir->esq == NULL) {
+    No *aux;
+    raiz->valor = dir->valor; // substituição
 
-		// removemos quem estava à direita
-		aux = dir;
-		dir = dir->dir;
-		free(aux);
-	} else //continuamos procurando o menor
-		substituiMenorDireita(raiz, dir->esq);
+    // removemos quem estava à direita
+    aux = dir;
+    dir = dir->dir;
+    free(aux);
+  } else // continuamos procurando o menor
+    substituiMenorDireita(raiz, dir->esq);
 }
 
 // Impressões --------------------------------------------
 
 // Imprime a árvore em pré-ordem
-void imprimirPreOrdem(No *raiz){
-	if(raiz != NULL){
-		printf("%d ", raiz->valor);
-		imprimirPreOrdem(raiz->esq);
-		imprimirPreOrdem(raiz->dir);
-	}
+void imprimirPreOrdem(No *raiz) {
+  if (raiz != NULL) {
+    printf("%d ", raiz->valor);
+    imprimirPreOrdem(raiz->esq);
+    imprimirPreOrdem(raiz->dir);
+  }
 }
 
 // Imprime a árvore em in-ordem
-void imprimirInOrdem(No *raiz){
-	if(raiz != NULL){
-		imprimirPreOrdem(raiz->esq);
-		printf("%d ", raiz->valor);
-		imprimirPreOrdem(raiz->dir);
-	}
+void imprimirInOrdem(No *raiz) {
+  if (raiz != NULL) {
+    imprimirInOrdem(raiz->esq);
+    printf("%d ", raiz->valor);
+    imprimirInOrdem(raiz->dir);
+  }
 }
 
 // Imprime a árvore em pós-ordem
-void imprimirPosOrdem(No *raiz){
-	if(raiz != NULL){
-		imprimirPreOrdem(raiz->esq);
-		imprimirPreOrdem(raiz->dir);
-		printf("%d ", raiz->valor);
-	}	
+void imprimirPosOrdem(No *raiz) {
+  if (raiz != NULL) {
+    imprimirPosOrdem(raiz->esq);
+    imprimirPosOrdem(raiz->dir);
+    printf("%d ", raiz->valor);
+  }
 }
-
-
