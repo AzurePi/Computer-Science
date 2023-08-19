cfc45bdc48e0b97b0ad1a28f184dae698166c51a
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2023-08-19 18:04:03 -0300

    progress on the TextBreaker

--- 4º Semestre/Probablilidade e Estatística/TextBreaker.java
+++ 4º Semestre/Probablilidade e Estatística/TextBreaker.java
@@ -1,9 +1,11 @@
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class TextBreaker {
@@ -12,79 +14,72 @@ public class TextBreaker {
         ArrayList<String> palavras;
         StringBuilder trecho = new StringBuilder();
         Path diretorioDestino;
-        File destino;
+        String fileName;
+        Path file;
+        int i, tamanhoTexto, projecao; //controla que linha do arquivo de origem está sendo lida e seu tamanho total
         int n; //controla o nome do arquivo txt de cada trecho
-        int inicio, fim; //índices da palavra no trecho
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Quebra um arquivo txt em trechos de 4000 caracteres (sem quebrar palavras ao meio), e salva esse textos em arquivos txt no endereço especificado");
 
         try {
-            texto = lerTexto();
+            texto = lerTexto(sc);
         } catch (IOException e) {
             System.out.println("ERRO: falha ao ler o arquivo de origem");
             throw new RuntimeException(e);
         }
-        diretorioDestino = lerDestino();
-
+        diretorioDestino = lerDestino(sc);
 
         //transformamos a String texto em uma array de strings, em que cada String é uma única palavra
         palavras = new ArrayList<>(Arrays.asList(texto.split(" ")));
 
-        n = 0;
+        n = i = 0;
+        tamanhoTexto = palavras.size();
+        while (i < tamanhoTexto) {
+            projecao = trecho.length() + palavras.get(i).length();
 
-        while (!palavras.isEmpty()) {
-            trecho.append(palavras.get(0));
+            //não salva o último trecho! descobrir porque, alguma coisa com os ifs
 
-            //se extrapolamos 4000 caracteres
-            if (trecho.length() > 4000) {
-                //determina os índices inicial e final da última palavra inserida
-                inicio = trecho.indexOf(palavras.get(0));
-                fim = inicio + palavras.get(0).length();
-
-                trecho.delete(inicio, fim); //remove a última palavra inserida
-
+            if (projecao <= 4000) {
+                trecho.append(palavras.get(i));
+                trecho.append(" ");
+                i++;
+            } else { //já chegamos ao tamanho máximo
                 try {
-                    destino = new File(diretorioDestino.toFile(), n + ".txt");
-                    salvar(trecho, destino);
+                    fileName = n + ".txt";
+                    file = Paths.get(diretorioDestino.toString(), fileName);
+                    salvar(trecho, file);
                     n++;
+                    trecho = new StringBuilder(); //começamos a montar um novo trecho
                 } catch (IOException e) {
                     System.out.println("ERRO: falha no salvamento do arquivo");
                     throw new RuntimeException(e);
                 }
-            } else //se não extrapolamos 4000 caracteres
-                palavras.remove(0); //removemos a palavra inserida da lista de palavras
-
-            trecho.append(" "); //colocamos um espaço após a palavra
+            }
         }
+
+        sc.close();
     }
 
-    private static void salvar(StringBuilder trecho, File destino) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter(destino));
+    private static String lerTexto(Scanner sc) throws IOException {
+        System.out.print("Endereço do arquivo de origem: ");
+        String aux = sc.nextLine();
+        Path origem = Paths.get(aux.substring(1, aux.length() - 1)); //remove as aspas coladas no caminho
+
+        List<String> lines = Files.readAllLines(origem);
+        return String.join(System.lineSeparator(), lines);
+    }
+
+    public static Path lerDestino(Scanner sc) {
+        System.out.print("Diretório de destino: ");
+        String aux = sc.nextLine();
+        return Paths.get(aux.substring(1, aux.length() - 1)); //remove as aspas coladas no caminho
+    }
+
+    private static void salvar(StringBuilder trecho, Path destino) throws IOException {
+        Files.createFile(destino);
+        BufferedWriter writer = Files.newBufferedWriter(destino);
         writer.write(trecho.toString());
         writer.close();
-        System.out.println("salvar() ok");
-    }
-
-    public static Path lerDestino() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Diretório de destino: ");
-        Path destino = Paths.get(sc.nextLine());
-        sc.close();
-        System.out.println("lerDestino() ok");
-        return destino;
-    }
-
-    private static String lerTexto() throws IOException {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Endereço do arquivo de origem: ");
-        Path path = Paths.get(sc.nextLine());
-        sc.close();
-
-        BufferedReader reader = Files.newBufferedReader(path);
-        String lines = reader.lines().toString();
-        reader.close();
-
-        System.out.println("lerTexto() ok");
-        return lines;
     }
 }
