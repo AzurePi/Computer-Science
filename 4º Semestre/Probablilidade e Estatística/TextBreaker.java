df5a16da8059771b4efaa2129213c15aac3a8b2f
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2023-08-22 21:31:30 -0300

    finished the TextBreaker

--- 4º Semestre/Probablilidade e Estatística/TextBreaker.java
+++ 4º Semestre/Probablilidade e Estatística/TextBreaker.java
@@ -3,21 +3,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class TextBreaker {
     public static void main(String[] args) {
         String texto;
         ArrayList<String> palavras;
+        StringTokenizer tokenizer;
         StringBuilder trecho;
-        Path diretorioDestino, fileDestino;
-        String fileName;
+        Path diretorioDestino;
         int i, tamanhoTexto, projecao; //controla que linha do arquivo de origem está sendo lida e seu tamanho total
-        int a; //controla o nome do arquivo txt de cada trecho
-        int continuar; //controla se mais arquivos serão processados
+        int a, totalArquivos; //controla o nome do arquivo txt de cada trecho, e a quantidade total de arquivos
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Quebra um arquivo txt em trechos de 4000 caracteres (sem quebrar palavras ao meio), e salva esse textos em arquivos txt no endereço especificado");
@@ -28,16 +24,21 @@ public class TextBreaker {
                 texto = lerTexto(sc);
 
                 //transformamos a String texto em uma array de strings, em que cada String é uma única palavra
-                palavras = new ArrayList<>(Arrays.asList(texto.split(" ")));
-                tamanhoTexto = palavras.size();
+                palavras = new ArrayList<>();
+                tokenizer = new StringTokenizer(texto, " \t\r\f"); //usa um tokenizador que não considera \n como delimitador
+                while (tokenizer.hasMoreTokens())
+                    palavras.add(tokenizer.nextToken());
+
+                tamanhoTexto = palavras.size(); //calcula o número de palavras no texto
+                totalArquivos = (palavras.stream().mapToInt(String::length).sum() + 3999) / 4000; //calcula o total de arquivos que serão gerados
             } catch (IOException e) {
                 System.out.println("ERRO: falha ao ler o arquivo de origem");
                 throw new RuntimeException(e);
             }
             diretorioDestino = lerDestino(sc);
 
-            trecho = new StringBuilder(); //inicializamos o trecho de 4000 caracteres
             a = i = 0; //zera o contador de arquivos e o contador de palavras
+            trecho = new StringBuilder("[" + a + "/" + totalArquivos + "]\n"); //inicializamos o trecho de 4000 caracteres
             while (i < tamanhoTexto) {
                 projecao = trecho.length() + palavras.get(i).length();
 
@@ -46,34 +47,17 @@ public class TextBreaker {
                     trecho.append(palavras.get(i)).append(" ");
                     i++; //passamos para a próxima palavra
                 } else { //já chegamos ao tamanho máximo do trecho
-                    try {
-                        fileName = a + ".txt";
-                        fileDestino = Paths.get(diretorioDestino.toString(), fileName);
-                        salvar(trecho, fileDestino); //salvamos o trecho em um arquivo
-                        a++; //passamos para o próximo arquivo
-                        trecho = new StringBuilder(); //começamos a montar um novo trecho
-                    } catch (IOException e) {
-                        System.out.println("ERRO: falha no salvamento do arquivo");
-                        throw new RuntimeException(e);
-                    }
+                    salvarTrecho(diretorioDestino, trecho.toString(), a);
+                    a++; //passamos para o próximo arquivo
+                    trecho = new StringBuilder("[" + a + "/" + totalArquivos + "]\n"); //começamos a montar um novo trecho
                 }
             }
-
             //se ainda há informações a salvar no trecho
-            if (trecho.length() > 0){
-                try {
-                    fileName = a + ".txt";
-                    fileDestino = Paths.get(diretorioDestino.toString(), fileName);
-                    salvar(trecho, fileDestino);
-                } catch (IOException e) {
-                    System.out.println("ERRO: falha no salvamento do arquivo");
-                    throw new RuntimeException(e);
-                }
-            }
+            if (!trecho.isEmpty())
+                salvarTrecho(diretorioDestino, trecho.toString(), a);
 
-            System.out.print("Digite 1 para continuar com outro arquivo ");
-            //continuar = sc.nextInt();
-        } while (sc.nextInt() == 1);
+            System.out.print("Digite OK para continuar com outro arquivo ");
+        } while (sc.nextLine().equalsIgnoreCase("OK"));
         sc.close();
     }
 
@@ -89,10 +73,21 @@ public class TextBreaker {
         return Paths.get(validar(sc.nextLine()));
     }
 
-    public static void salvar(StringBuilder trecho, Path destino) throws IOException {
+    public static void salvarTrecho(Path diretorioDestino, String trecho, int a) {
+        try {
+            String fileName = a + ".txt";
+            Path fileDestino = Paths.get(diretorioDestino.toString(), fileName);
+            salvar(trecho, fileDestino);
+        } catch (IOException e) {
+            System.out.println("ERRO: falha no salvamento do arquivo");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void salvar(String trecho, Path destino) throws IOException {
         Files.createFile(destino);
         BufferedWriter writer = Files.newBufferedWriter(destino);
-        writer.write(trecho.toString());
+        writer.write(trecho);
         writer.close();
     }
 
